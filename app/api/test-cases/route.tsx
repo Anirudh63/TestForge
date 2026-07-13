ea7fe5c139f62af61a305565c1d8d2ff6b10d9f1
Anirudh Dhage <anirudhdhage72@gmail.com>
2026-07-13 14:08:22 +0530

    Generate Test Cases and Display

---
+++ app/api/test-cases/route.tsx
@@ -0,0 +1,24 @@
+import { db } from "@/db";
+import { TestCasesTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+        const searchparams = new URL(req.url).searchParams;
+        const repoId = searchparams.get('repoId');
+
+        if (!repoId) {
+            return NextResponse.json({ error: 'repoId is required' }, { status: 400 });
+        }
+
+        const result = await db.select().from(TestCasesTable).where(
+            eq(TestCasesTable.repoId, repoId)
+        );
+
+        return NextResponse.json(result);
+    } catch (error: any) {
+        console.error("Error fetching test cases:", error);
+        return NextResponse.json({ error: error.message || "Failed to fetch test cases" }, { status: 500 });
+    }
+}
